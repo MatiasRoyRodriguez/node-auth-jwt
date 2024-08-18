ff5e6256d4843849d9a6f5f1b723735919215439
Matias Roy Rodriguez <matiasrr980@gmail.com>
2024-08-18 14:05:06 -0300

    chore: remove console logs

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,7 +5,5 @@ import jwt from 'jsonwebtoken'
 const JWT_SECRET = process.env.JWT_SECRET || 'default-secret'
 
 export const generateToken = (user: User): string => {
-    console.log(user)
-    console.log(JWT_SECRET)
     return jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, { expiresIn: '1h' })
 }
